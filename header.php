d50f1bc397d02e8c513e6bc646db58ee6cea3077
Adam Waselnuk <adam.waselnuk@gmail.com>
2015-06-06 09:10:33 -0400

    Setting up type vars.

--- header.php
+++ header.php
@@ -49,7 +49,12 @@
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<h1 id="logo" class="ctr-logo" itemscope itemtype="http://schema.org/Organization">
+            <a class="logo" href="<?php echo home_url(); ?>" rel="nofollow">
+              <?php bloginfo('name'); ?><br>
+              <span class="logo-description"><?php bloginfo('description'); ?></span>
+            </a>
+          </h1>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
