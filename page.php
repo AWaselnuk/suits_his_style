3a3d29145dffa90d6930746a4ffc770ff525fada
Adam Waselnuk <adam.waselnuk@gmail.com>
2015-07-01 09:27:43 -0400

    Refining page styles and template.

--- page.php
+++ page.php
@@ -22,7 +22,7 @@ switch ($pagename) {
 <div class="page-banner background-image <?php echo $pageBannerClass ?>">
   <div class="row">
     <div class="col col-full">
-      <h1 class="page-banner__title"><h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+      <h1 class="page-banner__title"><?php the_title(); ?></h1>
       <p class="page-banner__text"><?php echo $pageBannerText ?></p>
     </div>
   </div>
