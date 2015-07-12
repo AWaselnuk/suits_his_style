2a7f838ef9f0713c34e0b14950e0f48caf7c0626
Adam Waselnuk <adam.waselnuk@gmail.com>
2015-07-12 09:17:17 -0400

    Styling post listing.

--- index.php
+++ index.php
@@ -9,40 +9,33 @@
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+			<div id="post-item-<?php the_ID(); ?>" class="post-item">
 
-				<header class="article-header">
-
-					<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-					<p class="byline entry-meta vcard">
-	                                                              <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-	             								/* the time the post was published */
-	             								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-	             								/* the author of the post */
-	             								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-	          							); ?>
+				<header class="post-item-header">
+					<h2 class="post-item-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+					<p class="post-item-time-and-author">
+            <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
+ 								/* the time the post was published */
+ 								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
+ 								/* the author of the post */
+ 								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+						); ?>
 					</p>
-
 				</header>
 
-				<section class="entry-content cf">
+				<section class="post-item-content">
 					<?php the_excerpt(); ?>
 				</section>
 
-				<footer class="article-footer cf">
-					<p class="footer-comment-count">
-						<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
-					</p>
+				<footer class="post-item-footer">
+					<!-- <p class="post-item-footer__comment-count">
+						<?php// comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
+					</p> -->
 
-
-	       	<?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
-
-	        <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-
+	       	<?php printf( '<p class="post-item-footer__category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
+	        <?php the_tags( '<p class="post-item-footer__tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
 				</footer>
-
-			</article>
+			</div>
 
 			<?php endwhile; ?>
 
@@ -50,20 +43,14 @@
 
 			<?php else : ?>
 
-					<article id="post-not-found" class="hentry cf">
-							<header class="article-header">
-								<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-						</header>
-							<section class="entry-content">
-								<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-						</section>
-						<footer class="article-footer">
-								<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
-						</footer>
-					</article>
+				<article id="post-not-found" class="row row-narrow">
+				  <div class="col col-full">
+				    <h2>Oops! Post not found.</h2>
+				    <p><a href="<?php echo site_url() . '/whats-new' ?>">View all articles.</a></p>
+				  </div>
+				</article>
 
 			<?php endif; ?>
-
 
 		</main>
 
