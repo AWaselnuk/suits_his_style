9e6475b058cf5b0301a8dd80115b201402b05611
Adam Waselnuk <adam.waselnuk@gmail.com>
2015-07-12 09:57:08 -0400

    Adjusting sidebar and form styles.

--- searchform.php
+++ searchform.php
@@ -1,8 +1,9 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div>
-        <label for="s" class="screen-reader-text"><?php _e('Search for:','bonestheme'); ?></label>
-        <input type="search" id="s" name="s" value="" />
-
-        <button type="submit" id="searchsubmit" ><?php _e('Search','bonestheme'); ?></button>
+<form role="search" method="get" id="searchform" class="form-inline" action="<?php echo home_url( '/' ); ?>">
+  <div>
+    <label for="s" class="screen-reader-text"><?php _e('Search for:','bonestheme'); ?></label>
+    <div class="form-inline__container">
+      <input class="form-inline__input" type="search" id="s" name="s" value="" placeholder="Enter your search..."/>
+      <button type="submit" id="searchsubmit" class="btn btn-primary form-inline__btn"><?php _e('Search','bonestheme'); ?></button>
     </div>
+  </div>
 </form>
